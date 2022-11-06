a266a859be796e0e0da859de750a5607344d21ae
JamesJPorter <porterjjames@gmail.com>
2022-11-05 21:32:30 -0700

    main menu and logic for selection responses started

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 
 // Connect to database
@@ -31,10 +32,42 @@ const db = mysql.createConnection(
             ]
         }
     ]
+    inquirer.prompt(home).then((data) => {
+        console.log('data', data)
+        const userAction = data.Menu;
+        console.log('userAction', userAction)
+        if (userAction === 'View All Employees'){
+            console.log(data)
+            viewAllEmployees()
+        } else if (userAction === 'Add Employee') {
+            console.log('Add Employee selected')
+        } else if (userAction === 'Update Employee Role'){
+            console.log('Update Employee role selected')
+        } else if (userAction === 'View All Roles'){
+            console.log('View All Roles selected')
+        } else if (userAction === 'View All Departments'){
+            console.log('View All Departments selected')
+        } else if (userAction === 'Add Department'){
+            console.log('Add Department selected')
+        } else {
+            mainMenu();
+        }
+    })
   }
 
   function viewAllEmployees(){
-    db.query('SELECT * FROM employee', function(err, result))
+    db.query('SELECT * FROM employee', function(err, results){
+        console.log(results)
+        const employeeString = JSON.stringify(results)
+        cTable.getTable(results)
+    })
+  }
+
+  function viewAllDepartments(){
+    db.query('SELECT * FROM department', function(err, results){
+        console.log(results)
+        
+    })
   }
 
   function init(){
