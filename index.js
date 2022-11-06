8faa8aece3ec990295991fa2fcf862e7ad8cae15
JamesJPorter <porterjjames@gmail.com>
2022-11-06 01:08:05 -0700

    working on adding new employee record

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
-const myPromise = new Promise((res, rej) => {
-    setTimeout(() => {
-        resolve()
-    })
-})
 
-
-// Connect to database
+//Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -47,10 +41,12 @@ const db = mysql.createConnection(
             viewAllEmployees()
         } else if (userAction === 'Add Employee') {
             console.log('Add Employee selected')
+            addEmployee();
         } else if (userAction === 'Update Employee Role'){
             console.log('Update Employee role selected')
         } else if (userAction === 'View All Roles'){
             console.log('View All Roles selected')
+            viewAllRoles();
         } else if (userAction === 'View All Departments'){
             console.log('View All Departments selected')
             viewAllDepartments();
@@ -60,25 +56,60 @@ const db = mysql.createConnection(
             mainMenu();
         }
     })
-  }
+  };
 
-  let viewAllEmployees = new Promise((res, rej) => {
-    res(db.query('SELECT * FROM employee', function(err, results){
+  function viewAllEmployees(){
+    db.query('SELECT * FROM employee', function(err, results){
+        if (err) throw err;
         console.log(cTable.getTable(results))
-    }));
-    rej(console.log(err));
-  })
-
-  viewAllEmployees.then(
-    mainMenu()
-  )
+        mainMenu();
+    })
+  }
 
   function viewAllDepartments(){
     db.query('SELECT * FROM department', function(err, results){
         if (err) throw err;
         console.log(cTable.getTable(results))
+        mainMenu();
     })
-    mainMenu();
+  }
+
+  function viewAllRoles(){
+    db.query('SELECT * FROM role', function(err, results){
+        if (err) throw err;
+        console.log(cTable.getTable(results))
+        mainMenu();
+    })
+  }
+
+  function addEmployee(){
+    const newEmployee = [
+        {
+            type: 'input', 
+            name: 'first_name', 
+            message: 'Enter employees first name',
+        }, 
+        {
+            type: 'input', 
+            name: 'last_name', 
+            message: 'Enter employees last name',
+        }, 
+        {
+            type: 'input', 
+            name: 'role_id', 
+            message: 'Enter employees role ID',
+        },
+        {
+            type: 'input', 
+            name: 'manager_id', 
+            message: `Enter the employees managers' ID`,
+        },
+    ]
+    inquirer.prompt(newEmployee).then((data) => {
+        console.log(cTable.getTable(data))
+        db.query(`INSERT INTO 'employee'('first_name', 'last_name', role_id, manager_id) VALUES(${data.first_name}, ${data.last_name}, ${data.role_id}, ${data.manager_id});`)
+        viewAllEmployees();
+    })
   }
 
   function init(){
